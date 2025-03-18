754e1f84223a288636a6fd804ac763bfce8c42b8
Alistair Wheeler <awheeler@simplicite.fr>
2025-03-18 16:56:22 +0100

    Add mermaid

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,47 +4,50 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Simplicité Documentation',
-  favicon: 'img/favicon.svg',
+  title: "Simplicité Documentation",
+  favicon: "img/favicon.svg",
 
   // Set the production url of your site here
-  url: 'https://simplicitesoftware.github.io/',
+  url: "https://simplicitesoftware.github.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus/',
+  baseUrl: "/docusaurus/",
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'simplicitesoftware', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "simplicitesoftware", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
 
-  onBrokenLinks: 'warn',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'fr',
-    locales: ['fr'],
+    defaultLocale: "fr",
+    locales: ["fr"],
   },
-
+  markdown: {
+    mermaid: true,
+  },
+  themes: ["@docusaurus/theme-mermaid"],
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
-          showLastUpdateTime: true
+          sidebarPath: "./sidebars.js",
+          showLastUpdateTime: true,
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -53,26 +56,33 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
+      colorMode: {
+        defaultMode: "dark",
+        disableSwitch: false,
+      },
       navbar: {
         logo: {
-          alt: 'Simplicité logo',
-          src: 'img/Simplicite_Logo_Black.svg',
-          srcDark: 'img/Simplicite_Logo_White.svg',
+          alt: "Simplicité logo",
+          src: "img/Simplicite_Logo_Black.svg",
+          srcDark: "img/Simplicite_Logo_White.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
-          }, 
-          {to: '/docs/category/simplicité-configuration-objects', label: 'Documentation', position: 'left'},
-          {to: '/docs/category/release-notes', label: 'Versions', position: 'left'},
-          /*{
-            type: 'search',
-            position: 'right',
-          },*/
+            type: "docSidebar",
+            sidebarId: "tutorialSidebar",
+            position: "left",
+            label: "Tutorial",
+          },
+          {
+            to: "/docs/category/simplicité-configuration-objects",
+            label: "Documentation",
+            position: "left",
+          },
+          {
+            to: "/docs/category/release-notes",
+            label: "Versions",
+            position: "left",
+          },
         ],
       },
       prism: {
@@ -81,34 +91,35 @@ const config = {
       },
       algolia: {
         // The application ID provided by Algolia
-        appId: 'Y8TW4DY3DN',
-  
+        appId: "Y8TW4DY3DN",
+
         // Public API key: it is safe to commit it
-        apiKey: 'f85735dfeaa4cd114b51e2b88ded5937',
-  
-        indexName: 'simplicitesoftwareio',
-  
+        apiKey: "f85735dfeaa4cd114b51e2b88ded5937",
+        indexName: "simplicitesoftwareio",
+
         // Optional: see doc section below
         contextualSearch: true,
-  
+
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         //externalUrlRegex: 'external\\.com|domain\\.com',
-  
+
         // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      /*  replaceSearchResultPathname: {
+        /*  
+        replaceSearchResultPathname: {
           from: '/docs/', // or as RegExp: /\/docs\//
           to: '/',
-        },*/
-  
+        },
+        */
+
         // Optional: Algolia search parameters
         searchParameters: {},
-  
+
         // Optional: path for search page that enabled by default (`false` to disable it)
-        searchPagePath: 'search',
-  
+        searchPagePath: "search",
+
         // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
         insights: false,
-  
+
         //... other Algolia params
       },
     }),
