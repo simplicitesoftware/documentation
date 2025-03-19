cb2590603758fb73c994274e1f9aefab32301955
David AZOULAY <44975645+dazoulay-simplicite@users.noreply.github.com>
2025-03-19 18:21:04 +0100

    Test custom URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,10 +14,10 @@ const config = {
   favicon: "img/favicon.svg",
 
   // Set the production url of your site here
-  url: "https://simplicitesoftware.github.io/",
+  url: "https://docsnew.simplicite.io/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docusaurus/",
+  baseUrl: "/",
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "simplicitesoftware", // Usually your GitHub org/user name.
